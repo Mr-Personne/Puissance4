8f7010e9cba4818e6e8275326c0466459fffe44c
Mr-Personne <h.sacha07@gmail.com>
2019-11-16 11:44:55 +0000

    -finalisation des fonctions verifWin (horizontale, verticale, diagonale gauche et droite), -rajout de jeton avec un .add au classList de la fonction 'ajoutJeton', -rajout de commentaires

--- js/human-versus.js
+++ js/human-versus.js
@@ -10,11 +10,11 @@ var JetonALaSuite = 0;
 
 
 var ajoutJeton = function (event) {
-    console.log("ajoutJeton");
-    //si l'emplacement est déja pris
-    if (event.target.classList.value == "jeton-rouge" || event.target.classList.value == "jeton-jaune") {
+    // console.log("ajoutJeton");
+    //si l'emplacement est déja pris :
+    if (event.target.classList[1] == "jeton-rouge" || event.target.classList[1] == "jeton-jaune") {
         console.log("not free space, go up");
-        // addOnTop(event);
+        // arrete la fonction
         return;
     }
     else {
@@ -27,58 +27,29 @@ var ajoutJeton = function (event) {
             player1 = true;
         }
 
-        event.target.classList = jetonActif;
+        // ajout un jeton (la class jeton de la couleur du joueur en cours)
+        // event.target.classList = jetonActif;
+        //needs to be a .add class so that we can both have .colonne classes and jeton classes
+        event.target.classList.add(jetonActif);
+        //active fonction qui rendra la case au dessus de celle qui vient d'etre clické clickable
         makeTopClickable(event);
     }
     console.log(" player1 ", player1);
-
+    // fonction qui vérifie si il y a des/une suite(s) gagnant
     verifWin(event);
 }
 
+//initialise la première ligne/la rend clickable
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 for (var i = 0; i < ligne6.length; i++) {
     console.log("ajoutJeton");
     ligne6[i].addEventListener("click", ajoutJeton);
-    // ligne6[i].addEventListener("click", function (event) {
-
-
-    //     //si l'emplacement est déja pris
-    //     if (event.target.classList.value == "jeton-rouge" || event.target.classList.value == "jeton-jaune") {
-    //         console.log("not free space, go up");
-    //         // addOnTop(event);
-    //         return;
-    //     }
-    //     else {
-    //         if (player1 == true) {
-    //             jetonActif = "jeton-rouge";
-    //             player1 = false;
-    //         }
-    //         else if (player1 == false) {
-    //             jetonActif = "jeton-jaune";
-    //             player1 = true;
-    //         }
-
-    //         event.target.classList = jetonActif;
-    //         makeTopClickable(event)
-    //     }
-
-    //     console.log(" player1 ", player1);
-
-    //     verifWin()
-
-    // });
 }
 
-// var rowNum = 6;
+
+
 //verif suite horizontale
 function verifHorizontale(event) {
-    // if (rowNum == undefined) {
-    //     var num = 6
-    //     console.log("num : ", num);
-    // }
-    // else {
-    //     var num = rowNum;
-    //     console.log("num : ", num);
-    // }
 
     var ligne6 = document.querySelectorAll(".ligne6 td");
     var ligne5 = document.querySelectorAll(".ligne5 td");
@@ -86,26 +57,25 @@ function verifHorizontale(event) {
     var ligne3 = document.querySelectorAll(".ligne3 td");
     var ligne2 = document.querySelectorAll(".ligne2 td");
     var ligne1 = document.querySelectorAll(".ligne1 td");
-    var lignesArray = [ligne6,ligne5,ligne4,ligne3,ligne2,ligne1];
+    var lignesArray = [ligne6, ligne5, ligne4, ligne3, ligne2, ligne1];
 
-    var rowNum = event.target.parentElement;
-    // console.log("lignes :  ", ligne6, ligne5, ligne4, ligne3, ligne2, ligne1);
-    // var currentRow = document.querySelectorAll(".ligne" + 6 + " td");
 
+    // première boucle qui nous permet de sélectionner chaques ligne contenu dans 'lignesArray'
     for (var i = 0; i < 6; i++) {
-        // currentRow = document.querySelectorAll(".ligne" + i + " td");
+
         var currentRow = lignesArray[i];
-        
-        console.log("current row ", currentRow);
-        for (var j = 0; j < 7; j++) {
-            if (currentRow[j].classList == "jeton-rouge" && currentRow[j + 1].classList == "jeton-rouge"
-                && currentRow[j + 2].classList == "jeton-rouge" && currentRow[j + 3].classList == "jeton-rouge") {
+        // console.log("current row ", currentRow);
+
+        // deuxieme boucle qui selectionne chaque case de la liste précédement selectionné
+        for (var j = 0; j < 4; j++) {
+            if (currentRow[j].classList[1] == "jeton-rouge" && currentRow[j + 1].classList[1] == "jeton-rouge"
+                && currentRow[j + 2].classList[1] == "jeton-rouge" && currentRow[j + 3].classList[1] == "jeton-rouge") {
 
                 alert("winner is red");
 
             }
-            else if (currentRow[j].classList == "jeton-jaune" && currentRow[j + 1].classList == "jeton-jaune"
-                && currentRow[j + 2].classList == "jeton-jaune" && currentRow[j + 3].classList == "jeton-jaune") {
+            else if (currentRow[j].classList[1] == "jeton-jaune" && currentRow[j + 1].classList[1] == "jeton-jaune"
+                && currentRow[j + 2].classList[1] == "jeton-jaune" && currentRow[j + 3].classList[1] == "jeton-jaune") {
 
                 alert("winner is yellow");
 
@@ -113,98 +83,211 @@ function verifHorizontale(event) {
         }
     }
 
-    // for (var i = 0; i < currentRow.length; i++) {
-    //     // console.log("currentRow[i]+123 ", currentRow[i], currentRow[i + 1], currentRow[i + 2], currentRow[i + 3])
-    //     //verif des jetons rouges a la suite horizontale gauche droite
-    //     if (currentRow[i].classList == "jeton-rouge" && currentRow[i + 1].classList == "jeton-rouge"
-    //         && currentRow[i + 2].classList == "jeton-rouge" && currentRow[i + 3].classList == "jeton-rouge") {
-
-    //         alert("winner is red");
-
-    //     }
-    //     else if (currentRow[i].classList == "jeton-jaune" && currentRow[i + 1].classList == "jeton-jaune"
-    //         && currentRow[i + 2].classList == "jeton-jaune" && currentRow[i + 3].classList == "jeton-jaune") {
-
-    //         alert("winner is yellow");
-
-    //     }
-    // }
+}
 
 
+//verif suite verticale
+function verifVerticale(event) {
+
+    var colonne7 = document.querySelectorAll(".colonne7");
+    var colonne6 = document.querySelectorAll(".colonne6");
+    var colonne5 = document.querySelectorAll(".colonne5");
+    var colonne4 = document.querySelectorAll(".colonne4");
+    var colonne3 = document.querySelectorAll(".colonne3");
+    var colonne2 = document.querySelectorAll(".colonne2");
+    var colonne1 = document.querySelectorAll(".colonne1");
+    var colonnesArray = [colonne7, colonne6, colonne5, colonne4, colonne3, colonne2, colonne1];
+    // console.log("col ", colonne7);
+
+
+    // première boucle qui nous permet de sélectionner chaques ligne contenu dans 'lignesArray'
+    for (var i = 0; i < colonnesArray.length; i++) {
+
+        var currentCol = colonnesArray[i];
+        // console.log("current col ", currentCol);
+
+        // deuxieme boucle qui selectionne chaque case de la liste précédement selectionné
+        for (var j = 0; j < 3; j++) {
+            if (currentCol[j].classList[1] == "jeton-rouge" && currentCol[j + 1].classList[1] == "jeton-rouge"
+                && currentCol[j + 2].classList[1] == "jeton-rouge" && currentCol[j + 3].classList[1] == "jeton-rouge") {
+
+                alert("winner is red");
+
+            }
+            else if (currentCol[j].classList[1] == "jeton-jaune" && currentCol[j + 1].classList[1] == "jeton-jaune"
+                && currentCol[j + 2].classList[1] == "jeton-jaune" && currentCol[j + 3].classList[1] == "jeton-jaune") {
+
+                alert("winner is yellow");
+
+            }
+        }
+    }
 
 }
-//verif suite verticale
+
 
 //verif suite diagonale "//" (gauche à droite)
-function verifDiagonaleGaucheDroite() {
-    var tableau = [];
+function verifDiagonaleGaucheDroite(event) {
 
-    for (var i = 0; i < trous.length; i++) {
-        tableau.push(trous[i]);
-    }
+    var ligne6 = document.querySelectorAll(".ligne6 td");
+    var ligne5 = document.querySelectorAll(".ligne5 td");
+    var ligne4 = document.querySelectorAll(".ligne4 td");
+    var ligne3 = document.querySelectorAll(".ligne3 td");
+    var ligne2 = document.querySelectorAll(".ligne2 td");
+    var ligne1 = document.querySelectorAll(".ligne1 td");
+    var lignesArray = [ligne6, ligne5, ligne4, ligne3, ligne2, ligne1];
 
-    for (var j = 0; j < tableau.length; j++) {
-        // tableau.push(trous[i]);
 
-        // //verif des jetons rouges a la suite horizontale gauche droite
-        // if (tableau[j].classList == "jeton-rouge") {
+    // première boucle qui nous permet de sélectionner chaques ligne contenu dans 'lignesArray'
+    for (var i = 0; i < 3; i++) {
 
-        //     console.log("tableau class", JetonALaSuite);
-        //     //faire une autre boucle de 4 pour verif si 4 a la suite
-        //     for (var k = 0; k <= 4; k++) {
-        //         // console.log("tableau[j + k]",tableau[j + k]);
-        //         // console.log("undefined test",tableau[j + k].classList);
-        //         if (tableau[j + k].classList.value == "jeton-rouge") {
-        //             console.log("[j + k] ==", j ," ",k);
-        //             console.log("tableau[j + k] ==", tableau[j + k]);
-        //             JetonALaSuite++;
-        //             console.log("jetonsuite", JetonALaSuite);
-        //         }
-        //         // else if (tableau[j + k].classList.value == "" || tableau[j + k].classList == undefined) {
-        //         //     console.log("undefined");
-        //         //     break;
-        //         // }
-        //     }
-        //     if (JetonALaSuite == 4) {
-        //         alert("winner is you");
-        //     }
-        // }
-        console.log("tableau[j]+123 ", tableau[j], tableau[j + 1], tableau[j + 2], tableau[j + 3])
-        //verif des jetons rouges a la suite horizontale gauche droite
-        if (tableau[j].classList == "jeton-rouge" && tableau[j + 1].classList == "jeton-rouge"
-            && tableau[j + 2].classList == "jeton-rouge" && tableau[j + 3].classList == "jeton-rouge") {
+        var currentRow = lignesArray[i];
+        // console.log("current row ", currentRow);
 
-            alert("winner is red");
+        // deuxieme boucle qui selectionne chaque case de la liste précédement selectionné
+        for (var j = 0; j < 4; j++) {
+            if (currentRow[j].classList[1] == "jeton-rouge") {
+                // console.log("currentRow 1: ", currentRow);
+                currentRow = lignesArray[i + 1];
 
+                if (currentRow[j + 1].classList[1] == "jeton-rouge") {
+                    // console.log("currentRow 2: ", currentRow);
+                    currentRow = lignesArray[i + 2];
+
+                    if (currentRow[j + 2].classList[1] == "jeton-rouge") {
+                        // console.log("currentRow 3: ", currentRow);
+                        currentRow = lignesArray[i + 3];
+
+                        if (currentRow[j + 3].classList[1] == "jeton-rouge") {
+                            // console.log("currentRow 4: ", currentRow);
+                            alert("winner is rouge");
+                        }
+
+                    }
+                }
+                //remet current row a sa position initial de la boucle
+                //ils étaient modif par les if précédents
+                currentRow = lignesArray[i];
+            }
+            else if (currentRow[j].classList[1] == "jeton-jaune") {
+                // console.log("currentRow 1: ", currentRow);
+                currentRow = lignesArray[i + 1];
+
+                if (currentRow[j + 1].classList[1] == "jeton-jaune") {
+                    // console.log("currentRow 2: ", currentRow);
+                    currentRow = lignesArray[i + 2];
+
+                    if (currentRow[j + 2].classList[1] == "jeton-jaune") {
+                        // console.log("currentRow 3: ", currentRow);
+                        currentRow = lignesArray[i + 3];
+
+                        if (currentRow[j + 3].classList[1] == "jeton-jaune") {
+                            // console.log("currentRow 4: ", currentRow);
+                            alert("winner is jaune");
+                        }
+
+                    }
+                }
+                //remet current row a sa position initial de la boucle
+                //ils étaient modif par les if précédents
+                currentRow = lignesArray[i];
+            }
         }
-        if (tableau[j].classList == "jeton-yellow" && tableau[j + 1].classList == "jeton-yellow"
-            && tableau[j + 2].classList == "jeton-yellow" && tableau[j + 3].classList == "jeton-yellow") {
-
-            alert("winner is yellow");
-
-        }
-
-
     }
-    console.log("test tableau ");
-    // console.log("tableau ", tableau)
 }
+
+
 //verif suite digonale "\\"  (droite à gauche)
+function verifDiagonaleDroiteGauche(event) {
+
+    var ligne6 = document.querySelectorAll(".ligne6 td");
+    var ligne5 = document.querySelectorAll(".ligne5 td");
+    var ligne4 = document.querySelectorAll(".ligne4 td");
+    var ligne3 = document.querySelectorAll(".ligne3 td");
+    var ligne2 = document.querySelectorAll(".ligne2 td");
+    var ligne1 = document.querySelectorAll(".ligne1 td");
+    var lignesArray = [ligne6, ligne5, ligne4, ligne3, ligne2, ligne1];
+
+
+    // première boucle qui nous permet de sélectionner chaques ligne contenu dans 'lignesArray'
+    for (var i = 0; i < 3; i++) {
+
+        var currentRow = lignesArray[i];
+        // console.log("current row ", currentRow);
+
+        // deuxieme boucle qui selectionne chaque case de la liste précédement selectionné
+        //commence à la droite de la row et selectionne 4 cases de droite a gauche
+        for (var j = currentRow.length-1; j > 2; j--) {
+            if (currentRow[j].classList[1] == "jeton-rouge") {
+                console.log("currentRow 1: ", currentRow);
+                currentRow = lignesArray[i + 1];
+
+                if (currentRow[j - 1].classList[1] == "jeton-rouge") {
+                    console.log("currentRow 2: ", currentRow);
+                    currentRow = lignesArray[i + 2];
+
+                    if (currentRow[j - 2].classList[1] == "jeton-rouge") {
+                        console.log("currentRow 3: ", currentRow);
+                        currentRow = lignesArray[i + 3];
+
+                        if (currentRow[j - 3].classList[1] == "jeton-rouge") {
+                            console.log("currentRow 4: ", currentRow);
+                            alert("winner is rouge");
+                        }
+
+                    }
+                }
+                //remet current row a sa position initial de la boucle
+                //ils étaient modif par les 'if' précédents
+                currentRow = lignesArray[i];
+            }
+            else if (currentRow[j].classList[1] == "jeton-jaune") {
+                console.log("currentRow 1: ", currentRow);
+                currentRow = lignesArray[i + 1];
+
+                if (currentRow[j - 1].classList[1] == "jeton-jaune") {
+                    console.log("currentRow 2: ", currentRow);
+                    currentRow = lignesArray[i + 2];
+
+                    if (currentRow[j - 2].classList[1] == "jeton-jaune") {
+                        console.log("currentRow 3: ", currentRow);
+                        currentRow = lignesArray[i + 3];
+
+                        if (currentRow[j - 3].classList[1] == "jeton-jaune") {
+                            console.log("currentRow 4: ", currentRow);
+                            alert("winner is jaune");
+                        }
+
+                    }
+                }
+                //remet current row a sa position initial de la boucle
+                //ils étaient modif par les 'if' précédents
+                currentRow = lignesArray[i];
+            }
+        }
+    }
+}
 
 
 
 function verifWin(event) {
     verifHorizontale(event);
+    verifVerticale(event);
+    verifDiagonaleGaucheDroite(event);
+    verifDiagonaleDroiteGauche(event);
 }
 
 
 
 function makeTopClickable(event) {
-    var nextPos = event.target.parentElement.previousElementSibling.querySelectorAll("td");
-    //trouve l'index position de la case clické
-    var cellRowIndex = event.target.cellIndex;
+    if (event.target.parentElement.classList.value != "ligne1") {
+        // console.log(event.target.parentElement);
+        var nextPos = event.target.parentElement.previousElementSibling.querySelectorAll("td");
+        //trouve l'index position de la case clické
+        var cellRowIndex = event.target.cellIndex;
 
-    nextPos[cellRowIndex].addEventListener("click", ajoutJeton);
+        nextPos[cellRowIndex].addEventListener("click", ajoutJeton);
+    }
 }
 
 
@@ -212,31 +295,31 @@ function makeTopClickable(event) {
 
 
 //not used for now (or ever...?)
-function addOnTop(event) {
-    //trouve la prochaine row (a utiliser si on click sur un emplacement pris)
-    //et créer une array de td de la row au dessus de celle clické
-    var nextPos = event.target.parentElement.previousElementSibling.querySelectorAll("td");
-    //trouve l'index position de la case clické
-    var cellRowIndex = event.target.cellIndex;
-    // console.log("index =", nextPos);
-    // console.log("event ", event);
-    // console.log("current jeton: ", event.target.classList.value);
-    console.log("next available pos at : ", nextPos[cellRowIndex]);
+// function addOnTop(event) {
+//     //trouve la prochaine row (a utiliser si on click sur un emplacement pris)
+//     //et créer une array de td de la row au dessus de celle clické
+//     var nextPos = event.target.parentElement.previousElementSibling.querySelectorAll("td");
+//     //trouve l'index position de la case clické
+//     var cellRowIndex = event.target.cellIndex;
+//     // console.log("index =", nextPos);
+//     // console.log("event ", event);
+//     // console.log("current jeton: ", event.target.classList.value);
+//     console.log("next available pos at : ", nextPos[cellRowIndex]);
 
-    //use below if you want it to try and put a jeton on top automaticaly
-    if (player1 == true) {
-        jetonActif = "jeton-rouge";
-        player1 = false;
-    }
-    else if (player1 == false) {
-        jetonActif = "jeton-jaune";
-        player1 = true;
-    }
-    nextPos[cellRowIndex].classList = jetonActif;
-}
+//     //use below if you want it to try and put a jeton on top automaticaly
+//     if (player1 == true) {
+//         jetonActif = "jeton-rouge";
+//         player1 = false;
+//     }
+//     else if (player1 == false) {
+//         jetonActif = "jeton-jaune";
+//         player1 = true;
+//     }
+//     nextPos[cellRowIndex].classList = jetonActif;
+// }
 
-function test() {
-    console.log("test");
-}
+// function test() {
+//     console.log("test");
+// }
 
 
